9c50a446f2e4cb11eb2f9b0d26ed2ef6a08efc5a
Dan Li <danieljiajunli@gmail.com>
2020-08-19 17:44:38 -0400

    customized

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 var canvas = document.getElementById("paint");
+var input = document.getElementById("hex");
+
+canvas.style.position = "absolute";
+canvas.style.top = "50%";
+canvas.style.left = "50%";
+canvas.style.transform = "translate(-50%, -50%)";
+canvas.style.background = "white";
+
+canvas.width = 900;
+canvas.height = 900;
+
 var ctx = canvas.getContext("2d");
-var pos = {x: 0, y: 0};
+
+let painting = false;
+
+function startDraw(e) {
+  painting = true;
+  draw(e);
+}
+
+function stopDraw() {
+  painting = false;
+  ctx.beginPath();
+}
 
 function draw(e) {
-    if (e.buttons !== 1) return;
+  if (!painting) return;
 
-    var color = document.getElementById("hex").value;
+  var color = input.value;
 
-    ctx.beginPath();
+  var horizontal_offset = (window.innerWidth - canvas.width) / 2;
 
-    ctx.lineWidth = 20;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = color;
+  ctx.lineWidth = 10;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = color
 
-    ctx.moveTo(pos.x, pos.y);
-    setPosition(e);
-    ctx.lineTo(pos.x, pos.y);
+  ctx.lineTo((e.clientX - horizontal_offset), (e.clientY - 20));
+  ctx.stroke();
 
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo((e.clientX - horizontal_offset), (e.clientY - 20));
 }
 
-function setPosition(e) {
-
-}
-
-document.addEventListener("mousedown", draw);
-document.addEventListener("mouseenter", setPosition);
-document.addEventListener("mousemove", setPosition);
-
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mouseup", stopDraw);
+canvas.addEventListener("mousemove", draw);
